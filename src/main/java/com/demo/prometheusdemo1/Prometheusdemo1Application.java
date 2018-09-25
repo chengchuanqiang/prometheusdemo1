2b92a8366105ae42585e8a9aff5e28086aae9b64
chengchuanqiang <1625320316@qq.com>
2018-09-25 15:37:18 +0800

    1、添加spring-security配置，设置账户密码 2、使用FastJson作为转换器，需要显示配置MediaType

--- src/main/java/com/demo/prometheusdemo1/Prometheusdemo1Application.java
+++ src/main/java/com/demo/prometheusdemo1/Prometheusdemo1Application.java
@@ -1,14 +1,43 @@
 package com.demo.prometheusdemo1;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnablePrometheusEndpoint
 @EnableSpringBootMetricsCollector
-public class Prometheusdemo1Application {
+public class Prometheusdemo1Application extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+
+        // 初始化转换器
+        FastJsonHttpMessageConverter fastConvert = new FastJsonHttpMessageConverter();
+        // 初始化一个转换器配置
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        // 将配置设置给转换器并添加到HttpMessageConverter转换器列表中
+        fastConvert.setFastJsonConfig(fastJsonConfig);
+
+        // 配置返回MediaType
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConvert.setSupportedMediaTypes(supportedMediaTypes);
+
+        converters.add(fastConvert);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Prometheusdemo1Application.class, args);
